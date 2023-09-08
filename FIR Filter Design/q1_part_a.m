d92cad9fba0bf31173ca5b842631af158fa2d99a
Nuwan Kannangara <kannangaranv.20@uom.lk>
2023-09-08 11:32:16 +0530

    Update q1_part_a.m

--- FIR Filter Design/q1_part_a.m
+++ FIR Filter Design/q1_part_a.m
@@ -1,1 +1,11 @@
+%%Specification of filter
+fsamp = 4000;
+fcuts = [600 900 1400 1600];
+mags = [0 1 0];
+devs = [0.001259 0.01372 0.001259];
 
+%%impulse response of the filter
+[n,Wn,beta,ftype] = kaiserord(fcuts,mags,devs,fsamp);
+n = n + rem(n,2);
+hh = fir1(n,Wn,ftype,kaiser(n+1,beta),'noscale');
+impz(hh)
